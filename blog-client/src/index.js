298a10f38afc07526ef6d3fec2cdc38134a02c8f
Kevin Liao <kliao2016@gmail.com>
2024-01-07 10:53:38 -0700

    first commit

---
+++ blog-client/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Render into the div with id 'root' from public index.html
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
